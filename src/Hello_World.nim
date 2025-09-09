15e78aa382a6bdce5b429f0a28c3b33698b36f65
Mauro Galafassi <m.galafassi@qsa.net>
2025-09-09 12:02:59 +0200

    Aggiungere più commenti

--- src/Hello_World.nim
+++ src/Hello_World.nim
@@ -1,6 +1,7 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
 # E questo è una linea di commento aggiunta per vedere come funziona jj
+# JJ inizia a piacermi
 
 when isMainModule:
   echo("Hello, World!")
