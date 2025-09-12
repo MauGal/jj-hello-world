685f999ca6f8d0f3d2f0bbcd98cf2e16336ef8e5
MauGal <28858911+MauGal@users.noreply.github.com>
2025-09-12 10:28:54 +0200

    Update Hello_World.nim
    Tolto linea di commento

--- src/Hello_World.nim
+++ src/Hello_World.nim
@@ -1,6 +1,6 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
-# E questo è una linea di commento aggiunta per vedere come funziona jj
+
 # JJ inizia a piacermi
 
 # Una funzione che fa la somma di 2 numeri
